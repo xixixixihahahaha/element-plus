b5cb81cca84b47259b80587f8aac0b7e78d33563
xixixixihahahaha <136174220+xixixixihahahaha@users.noreply.github.com>
2023-06-10 21:04:27 +0800

    Update basic-usage.vue

--- docs/examples/popconfirm/basic-usage.vue
+++ docs/examples/popconfirm/basic-usage.vue
@@ -1,4 +1,13 @@
 <template>
+<el-popconfirm
+          title="确认删除该人员吗？"
+          @confirm="delPerson(scope.$index, scope.row)">
+          <el-button
+            size="mini"
+            type="danger"
+            slot="reference"
+            >删除</el-button>
+        </el-popconfirm>
   <el-popconfirm title="Are you sure to delete this?">
     <template #reference>
       <el-button>Delete</el-button>
